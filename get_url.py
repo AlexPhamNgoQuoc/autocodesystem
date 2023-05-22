3bdea4e2d54c9e3e26e713b9ed2e63f2432bbed6
AlexPhamNgoQuoc <131502621+AlexPhamNgoQuoc@users.noreply.github.com>
2023-05-22 10:17:26 +0700

    Create get_url.py

---
+++ get_url.py
@@ -0,0 +1,11 @@
+def get_url(mode: any):
+    if mode == 'tictactoe':
+        return 'https://raw.githubusercontent.com/AlexPhamNgoQuoc/autocodesystem/main/tictactoe.py'
+    elif mode == 'hangman':
+        return 'https://raw.githubusercontent.com/AlexPhamNgoQuoc/autocodesystem/main/hangman.py'
+    elif mode == 'numberguess':
+        return 'https://raw.githubusercontent.com/AlexPhamNgoQuoc/autocodesystem/main/numberguess.py'
+    elif mode == 'passwordgen':
+        return 'https://raw.githubusercontent.com/AlexPhamNgoQuoc/autocodesystem/main/randompasswordgenerator.py'
+    elif mode == 'studentmanagement':
+        return 'https://raw.githubusercontent.com/AlexPhamNgoQuoc/autocodesystem/main/studentmanagment.py'
